ff883e4e63b1226d0dcf0be151483c396069c24e
Mike Hays <haysmikethere@gmail.com>
2025-09-01 18:59:45 -0700

    Fix naming, add secrets to deploy

--- src/SubscribeForm.tsx
+++ src/SubscribeForm.tsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 
 const SubscribeForm = () => {
   useEffect(() => {
-    window.ml = { q: [["account", import.meta.env.VITE_MAILERLITE_ACCOUNT]] };
+    window.ml = {
+      q: [["account", import.meta.env.VITE_MAILERLITE_ACCOUNT_ID]],
+    };
     const script = document.createElement("script");
     script.src = "https://assets.mailerlite.com/js/universal.js";
     script.async = true;
